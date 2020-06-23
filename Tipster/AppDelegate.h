bad10d31dca27322aea8a4e7cd40d82cc0fbd6a1
Ruben Green <ragreen@fb.com>
2020-06-23 13:19:02 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Ruben Green on 6/23/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Ruben Green on 6/23/20.
+//  Copyright © 2020 Ruben Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
